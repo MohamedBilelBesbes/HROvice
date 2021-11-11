7836d344a65e42327c1b2de833e4f751e8a5d60b
MohamedBilelBesbes <65521409+MohamedBilelBesbes@users.noreply.github.com>
2021-11-11 16:13:13 +0100

    Update settings.py

--- hrovice/settings.py
+++ hrovice/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-kbt7&o&d=&*)q*83o2e4kx!e@qzgoa@%^0l%su*nk8_fdsh&1f'
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-kbt7&o&d=&*)q*83o2e4kx!e@qzgoa@%^0l%su*nk8_fdsh&1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
+#DEBUG = os.getenv('DEBUG')
 ALLOWED_HOSTS = ['*']
 
 

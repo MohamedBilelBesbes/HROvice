a54001b31b4f4f6b771b536b1860460176d99b2b
MohamedBilelBesbes <65521409+MohamedBilelBesbes@users.noreply.github.com>
2021-11-13 15:49:52 +0100

    Update views.py

--- hroviceapp/views.py
+++ hroviceapp/views.py
@@ -14,9 +14,7 @@ from pathlib import Path
 from django.http import StreamingHttpResponse
 from wsgiref.util import FileWrapper
 import mimetypes
-import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+from pathlib import Path
 
 at = 'Internship Attestation'
 class PDF(FPDF):
@@ -293,7 +291,7 @@ def make_attestation(request, pk):
     pdf.add_page()
     pdf.alias_nb_pages()
     pdf.chapter_body('media//content.txt', signer, name, school, cin, title, dateinit, dateend, dateofsign)
-    pdf.output('media//attestation_'+str(attestation.pk)+'_intern_'+str(intern.pk)+'.pdf')
+    pdf.output(Path('media//attestation_'+str(attestation.pk)+'_intern_'+str(intern.pk)+'.pdf'))
     #media_root = os.path.join(Path(__file__).resolve().parent.parent, 'media')
     filepath = 'media' + '//attestation_'+str(attestation.pk)+'_intern_'+str(intern.pk)+'.pdf'
     thefile = filepath

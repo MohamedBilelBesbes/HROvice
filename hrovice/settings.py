498452a66486f5ad510a08f87e3c09d9d0082719
Mohamed Bilel Besbes <bilel.besbes1@gmail.com>
2025-10-23 11:36:50 -0400

    Fixing database configuration issue

--- hrovice/settings.py
+++ hrovice/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-kbt7&o&d=&*)q*83o2e4kx!e@qzgoa@%^0l%su*nk8_fdsh&1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +79,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hrovice',
         'USER': 'postgres',
-        'PASSWORD': '123456',
         'HOST': 'db',
         'PORT': '5432',
     }
